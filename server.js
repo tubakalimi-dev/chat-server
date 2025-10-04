e11e7695649ea0f9c0a8ef362d2e9b5cef030b4a
Tuba Kalimi <tubakalimi@gmail.com>
2025-10-04 17:11:10 +0530

    Initial commit

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const io = socketIo(server, {
 });
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Store connected users
 const connectedUsers = new Map();
