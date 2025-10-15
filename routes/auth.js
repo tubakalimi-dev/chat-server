2fb757b0cfd96e0732ec166454485107ba21ba21
tubakalimi-dev <tubakalimi@gmail.com>
2025-10-15 19:54:15 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../routes/auth');
 const adminMiddleware = require('../middleware/admin');
 
 // Public routes
